27595c39b82a4d3b12c926f43d131425b55c8a4f
wadashuri <wada20010108@icloud.com>
2024-07-20 04:11:05 +0900

    軽微な修正

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue';
-import navigationMenu from './components/navigationMenu.vue';
 import Index from './components/memo/index.vue';
 import Show from './components/memo/show.vue';
 
 createApp({
     components: {
-        navigationMenu,
         Index,
         Show,
     }
